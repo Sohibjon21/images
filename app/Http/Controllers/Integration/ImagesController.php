5500fc65d55dc83411b7649f1a2a6ee0c2acac0c
Sohibjon21 <148609505+Sohibjon21@users.noreply.github.com>
2024-03-14 10:53:53 +0500

    api realized

--- app/Http/Controllers/Integration/ImagesController.php
+++ app/Http/Controllers/Integration/ImagesController.php
@@ -3,15 +3,32 @@
 namespace App\Http\Controllers\Integration;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\getInfoRequest;
 use App\Models\Images;
 use Illuminate\Http\Request;
 
 class ImagesController extends Controller
 {
-    public function getInfo()
+    public function getInfo(getInfoRequest $request)
     {
+        $data = $request->validated();
+
+        if (isset($data['id'])) {
+            $id = $data['id'];
+            $image = Images::find($id);
+            $image->makeHidden(['created_at']);
+            $image->makeHidden(['updated_at']);
+            return response()->json([
+                'info' => $image
+            ]);
+        }
+
+        $images = Images::all();
+        $images->makeHidden(['created_at']);
+        $images->makeHidden(['updated_at']);
+
         return response()->json([
-            'info'=>Images::all()
+            'info' => $images
         ]);
     }
 }
